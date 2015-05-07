1daa8b85cfac810c353a929d684ae3ca5e0a6289
Aleksi Sapon <alek42008@yahoo.com>
2015-05-07 18:27:14 -0400

    Add WorldEditingTest plugin

---
+++ Plugin/WorldEditingTest/src/main/java/org/spongepowered/cookbook/plugin/WorldEditingTest.java
@@ -0,0 +1,103 @@
+package org.spongepowered.cookbook.plugin;
+
+import com.flowpowered.math.vector.Vector2i;
+import com.flowpowered.math.vector.Vector3i;
+import com.google.common.base.Optional;
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.event.Subscribe;
+import org.spongepowered.api.event.state.ServerStartingEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.util.command.CommandCallable;
+import org.spongepowered.api.util.command.CommandException;
+import org.spongepowered.api.util.command.CommandResult;
+import org.spongepowered.api.util.command.CommandSource;
+import org.spongepowered.api.world.Chunk;
+import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.biome.BiomeTypes;
+import org.spongepowered.api.world.extent.Extent;
+
+import java.util.List;
+
+@Plugin(id = "worldeditingtest", name = "WorldEditingTest", version = "0.1")
+public class WorldEditingTest {
+
+    @Inject private Logger logger;
+    @Inject private Game game;
+    @Inject private PluginContainer instance;
+
+    @Subscribe
+    public void onServerStarting(ServerStartingEvent event) {
+        game.getCommandDispatcher().register(instance, new Commands(), "cstat");
+    }
+
+    private class Commands implements CommandCallable {
+
+        @Override
+        public List<String> getSuggestions(CommandSource source, String arguments) throws CommandException {
+            return null;
+        }
+
+        @Override
+        public boolean testPermission(CommandSource source) {
+            return source.hasPermission("command.sponge.cstat");
+        }
+
+        @Override
+        public Optional<Text> getShortDescription(CommandSource source) {
+            return Optional.of((Text) Texts.of("Gets a summary of the contents of chunk"));
+        }
+
+        @Override
+        public Optional<Text> getHelp(CommandSource source) {
+            return Optional.of(getUsage(source));
+        }
+
+        @Override
+        public Text getUsage(CommandSource source) {
+            return Texts.of("cstat [<x> <y> <z>]");
+        }
+
+        @Override
+        public Optional<CommandResult> process(CommandSource source, String arguments) throws CommandException {
+            if (source instanceof Player) {
+                final Player player = (Player) source;
+                final World world = player.getWorld();
+                final Vector3i blockPosition = getPosition(player, arguments.split(" "));
+                final Vector2i biomePosition = blockPosition.toVector2(true);
+                final Chunk chunk = world.getChunk(game.getServer().getChunkLayout().toChunk(blockPosition).get()).get();
+                final Extent extent = world;
+                player.sendMessage(Texts.of(extent.getBlock(blockPosition).getType().getName()));
+                player.sendMessage(Texts.of(extent.getBiome(biomePosition).getName()));
+                extent.setBiome(biomePosition, BiomeTypes.SWAMPLAND);
+                extent.setBlock(blockPosition, BlockTypes.GRASS.getDefaultState());
+                return Optional.of(CommandResult.success());
+            }
+            throw new CommandException(Texts.of("This command can only be executed by a player"));
+        }
+    }
+
+    private static Vector3i getPosition(Player player, String[] arguments) throws CommandException {
+        if (arguments.length == 1) {
+            return player.getLocation().getBlockPosition();
+        }
+        if (arguments.length != 3) {
+            throw new CommandException(Texts.of("Expected 3 arguments"));
+        }
+        final int x, y, z;
+        try {
+            x = Integer.parseInt(arguments[0]);
+            y = Integer.parseInt(arguments[1]);
+            z = Integer.parseInt(arguments[2]);
+        } catch (NumberFormatException exception) {
+            throw new CommandException(Texts.of("Expected 3 ints"));
+        }
+        return new Vector3i(x, y, z);
+    }
+}
