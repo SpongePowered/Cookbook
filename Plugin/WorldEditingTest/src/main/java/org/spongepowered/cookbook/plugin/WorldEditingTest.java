dc567e4d06d3ca60c03eadcbc8313fa2e8f64cd6
Aleksi Sapon <alek42008@yahoo.com>
2015-05-07 21:56:11 -0400

    Test block set/get type and add an argument to select either the world of the chunk as the extent

--- Plugin/WorldEditingTest/src/main/java/org/spongepowered/cookbook/plugin/WorldEditingTest.java
+++ Plugin/WorldEditingTest/src/main/java/org/spongepowered/cookbook/plugin/WorldEditingTest.java
@@ -34,7 +34,7 @@ public class WorldEditingTest {
 
     @Subscribe
     public void onServerStarting(ServerStartingEvent event) {
-        game.getCommandDispatcher().register(instance, new Commands(), "cstat");
+        game.getCommandDispatcher().register(instance, new Commands(), "edit");
     }
 
     private class Commands implements CommandCallable {
@@ -46,7 +46,7 @@ public class WorldEditingTest {
 
         @Override
         public boolean testPermission(CommandSource source) {
-            return source.hasPermission("command.sponge.cstat");
+            return source.hasPermission("command.sponge.edit");
         }
 
         @Override
@@ -61,40 +61,53 @@ public class WorldEditingTest {
 
         @Override
         public Text getUsage(CommandSource source) {
-            return Texts.of("cstat [<x> <y> <z>]");
+            return Texts.of("edit [c|w] [<x> <y> <z>]");
         }
 
         @Override
         public Optional<CommandResult> process(CommandSource source, String arguments) throws CommandException {
             if (source instanceof Player) {
+                final String[] split = arguments.split(" ");
+                final boolean useChunk;
+                final int index;
+                if (split[0].equals("c")) {
+                    useChunk = true;
+                    index = 1;
+                } else if (split[0].equals("w")) {
+                    useChunk = false;
+                    index = 1;
+                } else {
+                    useChunk = false;
+                    index = 0;
+                }
                 final Player player = (Player) source;
                 final World world = player.getWorld();
-                final Vector3i blockPosition = getPosition(player, arguments.split(" "));
+                final Vector3i blockPosition = getPosition(player, split, index);
                 final Vector2i biomePosition = blockPosition.toVector2(true);
                 final Chunk chunk = world.getChunk(game.getServer().getChunkLayout().toChunk(blockPosition).get()).get();
-                final Extent extent = world;
-                player.sendMessage(Texts.of(extent.getBlock(blockPosition).getType().getName()));
+                final Extent extent = useChunk ? chunk : world;
+                player.sendMessage(Texts.of(extent.getBlockType(blockPosition).getName()));
                 player.sendMessage(Texts.of(extent.getBiome(biomePosition).getName()));
                 extent.setBiome(biomePosition, BiomeTypes.SWAMPLAND);
-                extent.setBlock(blockPosition, BlockTypes.GRASS.getDefaultState());
+                extent.setBlockType(blockPosition, BlockTypes.GRASS);
                 return Optional.of(CommandResult.success());
             }
             throw new CommandException(Texts.of("This command can only be executed by a player"));
         }
     }
 
-    private static Vector3i getPosition(Player player, String[] arguments) throws CommandException {
-        if (arguments.length == 1) {
+    private static Vector3i getPosition(Player player, String[] arguments, int index) throws CommandException {
+        if (arguments.length - index <= 1) {
             return player.getLocation().getBlockPosition();
         }
-        if (arguments.length != 3) {
+        if (arguments.length - index != 3) {
             throw new CommandException(Texts.of("Expected 3 arguments"));
         }
         final int x, y, z;
         try {
-            x = Integer.parseInt(arguments[0]);
-            y = Integer.parseInt(arguments[1]);
-            z = Integer.parseInt(arguments[2]);
+            x = Integer.parseInt(arguments[index]);
+            y = Integer.parseInt(arguments[index + 1]);
+            z = Integer.parseInt(arguments[index + 2]);
         } catch (NumberFormatException exception) {
             throw new CommandException(Texts.of("Expected 3 ints"));
         }
