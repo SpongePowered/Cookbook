938958a89d6c1c22cf6741370511ef1a8466388f
Chris Sanders <zidane@outlook.com>
2015-06-12 20:00:19 -0500

    Update to be used to test unified world changes
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- Plugin/WorldsTest/src/main/java/org/spongepowered/cookbook/plugin/WorldsTest.java
+++ Plugin/WorldsTest/src/main/java/org/spongepowered/cookbook/plugin/WorldsTest.java
@@ -13,8 +13,7 @@ import org.spongepowered.api.Game;
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.entity.player.gamemode.GameModes;
 import org.spongepowered.api.event.Subscribe;
-import org.spongepowered.api.event.state.ServerStartingEvent;
-import org.spongepowered.api.event.world.WorldLoadEvent;
+import org.spongepowered.api.event.state.ServerAboutToStartEvent;
 import org.spongepowered.api.extra.skylands.SkylandsWorldGeneratorModifier;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
@@ -31,7 +30,6 @@ import org.spongepowered.api.world.DimensionTypes;
 import org.spongepowered.api.world.GeneratorTypes;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
-import org.spongepowered.api.world.gen.WorldGenerator;
 import org.spongepowered.api.world.storage.WorldProperties;
 
 import java.util.List;
@@ -43,122 +41,76 @@ public class WorldsTest {
     @Inject private Game game;
 
     @Subscribe
-    public void onServerStarting(ServerStartingEvent event) {
-        this.game.getRegistry().getWorldBuilder()
-            .name("nether")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.NETHER)
-            .generator(GeneratorTypes.NETHER)
-            .gameMode(GameModes.CREATIVE)
-            .build();
+    public void onServerAboutToStart(ServerAboutToStartEvent event) {
+        final SkylandsWorldGeneratorModifier skylandsModifier = new SkylandsWorldGeneratorModifier();
+        this.game.getRegistry().registerWorldGeneratorModifier(skylandsModifier);
 
         this.game.getRegistry().getWorldBuilder()
-            .name("end")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.END)
-            .generator(GeneratorTypes.END)
-            .gameMode(GameModes.CREATIVE)
-            .build();
+                .name("end")
+                .enabled(true)
+                .loadsOnStartup(true)
+                .keepsSpawnLoaded(true)
+                .dimensionType(DimensionTypes.END)
+                .generator(GeneratorTypes.END)
+                .gameMode(GameModes.CREATIVE)
+                .build();
 
         this.game.getRegistry().getWorldBuilder()
-            .name("overend")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.OVERWORLD)
-            .generator(GeneratorTypes.END)
-            .gameMode(GameModes.CREATIVE)
-            .build();
+                .name("nether")
+                .enabled(true)
+                .loadsOnStartup(true)
+                .keepsSpawnLoaded(true)
+                .dimensionType(DimensionTypes.NETHER)
+                .generator(GeneratorTypes.NETHER)
+                .gameMode(GameModes.CREATIVE)
+                .build();
 
         this.game.getRegistry().getWorldBuilder()
-            .name("netherover")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.NETHER)
-            .generator(GeneratorTypes.OVERWORLD)
-            .gameMode(GameModes.CREATIVE)
-            .build();
+                .name("skylands")
+                .enabled(true)
+                .loadsOnStartup(true)
+                .keepsSpawnLoaded(true)
+                .dimensionType(DimensionTypes.OVERWORLD)
+                .generator(GeneratorTypes.OVERWORLD)
+                .generatorModifiers(skylandsModifier)
+                .gameMode(GameModes.CREATIVE)
+                .build();
 
         this.game.getRegistry().getWorldBuilder()
-            .name("netherend")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.NETHER)
-            .generator(GeneratorTypes.END)
-            .gameMode(GameModes.CREATIVE)
-            .build();
-
-        this.game.getRegistry().getWorldBuilder()
-            .name("endover")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.END)
-            .generator(GeneratorTypes.OVERWORLD)
-            .gameMode(GameModes.CREATIVE)
-            .build();
-
-        this.game.getRegistry().getWorldBuilder()
-            .name("endnether")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.END)
-            .generator(GeneratorTypes.NETHER)
-            .gameMode(GameModes.CREATIVE)
-            .build();
-
-        game.getRegistry().getWorldBuilder()
-            .name("skylands")
-            .enabled(true)
-            .loadsOnStartup(true)
-            .keepsSpawnLoaded(true)
-            .dimensionType(DimensionTypes.OVERWORLD)
-            .generator(GeneratorTypes.OVERWORLD)
-            .gameMode(GameModes.CREATIVE)
-            .build();
+                .name("skyhell")
+                .enabled(true)
+                .loadsOnStartup(true)
+                .keepsSpawnLoaded(true)
+                .dimensionType(DimensionTypes.OVERWORLD)
+                .generator(GeneratorTypes.NETHER)
+                .generatorModifiers(skylandsModifier)
+                .gameMode(GameModes.CREATIVE)
+                .build();
 
         this.game.getCommandDispatcher().register(this, CommandSpec.builder()
-            .description(Texts.of("Teleports a player to another world"))
-            .arguments(seq(playerOrSource(Texts.of("target"), this.game), onlyOne(world(Texts.of("world"), this.game))))
-            .executor(new CommandExecutor() {
-                @Override
-                public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-                    final Optional<WorldProperties> optWorldProperties = args.getOne("world");
-                    final Optional<World> optWorld = game.getServer().getWorld(optWorldProperties.get().getWorldName());
-                    if (!optWorld.isPresent()) {
-                        throw new CommandException(Texts.of("World [", Texts.of(TextColors.AQUA, optWorldProperties.get().getWorldName()), "] "
-                            + "was not found."));
+                .description(Texts.of("Teleports a player to another world"))
+                .arguments(seq(playerOrSource(Texts.of("target"), this.game), onlyOne(world(Texts.of("world"), this.game))))
+                .permission("worldstest.command.tpworld")
+                .executor(new CommandExecutor() {
+                    @Override
+                    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+                        final Optional<WorldProperties> optWorldProperties = args.getOne("world");
+                        final Optional<World> optWorld = game.getServer().getWorld(optWorldProperties.get().getWorldName());
+                        if (!optWorld.isPresent()) {
+                            throw new CommandException(Texts.of("World [", Texts.of(TextColors.AQUA, optWorldProperties.get().getWorldName()), "] "
+                                    + "was not found."));
+                        }
+                        for (Player target : args.<Player>getAll("target")) {
+                            target.setLocation(new Location(optWorld.get(), optWorld.get().getProperties()
+                                    .getSpawnPosition()));
+                        }
+                        return CommandResult.success();
                     }
-                    for (Player target : args.<Player>getAll("target")) {
-                        target.transferToWorld(optWorld.get().getName(), optWorld.get().getProperties()
-                            .getSpawnPosition().toDouble());
-                    }
-                    return CommandResult.success();
-                }
-            })
-            .build()
-            , "tpworld");
+                })
+                .build()
+                , "tpworld");
 
         this.game.getCommandDispatcher().register(this, new CommandSpawn(this.game), "spawn");
-
-    }
-
-    @Subscribe
-    public void onWorldLoad(WorldLoadEvent event) {
-        final World world = event.getWorld();
-        if (world.getName().equals("skylands")) {
-            final SkylandsWorldGeneratorModifier skylandsModifier = new SkylandsWorldGeneratorModifier();
-            final WorldGenerator normalGenerator = world.getWorldGenerator();
-            skylandsModifier.modifyWorldGenerator(null, null, normalGenerator);
-            world.setWorldGenerator(normalGenerator);
-        }
     }
 
     private static class CommandSpawn implements CommandCallable {
@@ -192,6 +144,9 @@ public class WorldsTest {
         @Override
         public Optional<CommandResult> process(CommandSource source, String arguments) throws CommandException {
             if (source instanceof Player) {
+                if (!testPermission(source)) {
+                    return Optional.of(CommandResult.empty());
+                }
                 final Player player = (Player) source;
                 final String[] args = arguments.split(" ");
                 World world;
@@ -201,55 +156,52 @@ public class WorldsTest {
                 if (arguments.isEmpty()) {
                     world = player.getWorld();
                     spawnCoordinates = player.getWorld().getProperties().getSpawnPosition();
-                    player.setLocationSafely(new Location(world, spawnCoordinates.toDouble()));
+                    player.setLocation(new Location(world, spawnCoordinates.toDouble()));
                 } else {
-                    switch (args[0]) {
-                        case "-i":
-                            // (Player) /spawn -i
-                            if (args.length == 1) {
-                                world = player.getWorld();
-                                // (Player) /spawn -i <world_name>
+                    if ("-i".equals(args[0])) {
+                        // (Player) /spawn -i
+                        if (args.length == 1) {
+                            world = player.getWorld();
+                            // (Player) /spawn -i <world_name>
+                        } else {
+                            final Optional<World> optWorldCandidate = this.game.getServer().getWorld(args[1]);
+                            if (optWorldCandidate.isPresent()) {
+                                world = optWorldCandidate.get();
                             } else {
-                                final Optional<World> optWorldCandidate = this.game.getServer().getWorld(args[1]);
-                                if (optWorldCandidate.isPresent()) {
-                                    world = optWorldCandidate.get();
-                                } else {
-                                    source.sendMessage(Texts.of("World [", TextColors.AQUA, args[1], TextColors.WHITE, "] was not found"));
-                                    break;
-                                }
+                                source.sendMessage(Texts.of("World [", TextColors.AQUA, args[1], TextColors.WHITE, "] was not found"));
+                                return Optional.of(CommandResult.success());
                             }
+                        }
 
-                            spawnCoordinates = world.getProperties().getSpawnPosition();
+                        spawnCoordinates = world.getProperties().getSpawnPosition();
 
-                            source.sendMessage(Texts.of("World [", TextColors.AQUA, world.getName(), TextColors.WHITE,
+                        source.sendMessage(Texts.of("World [", TextColors.AQUA, world.getName(), TextColors.WHITE,
                                 "]: spawn -> x [", TextColors.GREEN, spawnCoordinates.getX(), TextColors.WHITE, "] | y [", TextColors.GREEN,
                                 spawnCoordinates.getY(), TextColors.WHITE, "] | z [", TextColors.GREEN, spawnCoordinates.getZ(), TextColors
-                                    .WHITE, "]."));
-                            break;
-                        case "-s":
-                            world = player.getWorld();
+                                        .WHITE, "]."));
+                    } else if ("-s".equals(args[0])) {
+                        world = player.getWorld();
 
-                            // (Player) /spawn -s <no args>
-                            if (args.length == 1) {
-                                spawnCoordinates = player.getLocation().getBlockPosition();
-                                // (Player) /spawn -s x y z
-                            } else {
-                                try {
-                                    spawnCoordinates = new Vector3i(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
-                                } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
-                                    source.sendMessage(Texts.of("Invalid spawn coordinates, must be in numeric format. Ex. /spawn -s 0 0 0."));
-                                    break;
-                                }
+                        // (Player) /spawn -s <no args>
+                        if (args.length == 1) {
+                            spawnCoordinates = player.getLocation().getBlockPosition();
+                            // (Player) /spawn -s x y z
+                        } else {
+                            try {
+                                spawnCoordinates = new Vector3i(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+                            } catch (Exception ex) {
+                                source.sendMessage(Texts.of("Invalid spawn coordinates, must be in numeric format. Ex. /spawn -s 0 0 0."));
+                                return Optional.of(CommandResult.success());
                             }
+                        }
 
-                            world.getProperties().setSpawnPosition(spawnCoordinates);
+                        world.getProperties().setSpawnPosition(spawnCoordinates);
 
-                            source.sendMessage(Texts.of("World [", TextColors.AQUA, world.getName(), TextColors.WHITE,
+                        source.sendMessage(Texts.of("World [", TextColors.AQUA, world.getName(), TextColors.WHITE,
                                 "]: spawn set to -> x [", TextColors.GREEN, spawnCoordinates.getX(), TextColors.WHITE, "] | y [", TextColors
-                                    .GREEN,
+                                        .GREEN,
                                 spawnCoordinates.getY(), TextColors.WHITE, "] | z [", TextColors.GREEN, spawnCoordinates.getZ(), TextColors
-                                    .WHITE, "]."));
-                            break;
+                                        .WHITE, "]."));
                     }
                 }
             }
