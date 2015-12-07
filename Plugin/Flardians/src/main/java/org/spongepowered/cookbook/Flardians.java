8a85e15c299c09a2e74ba7605372da1be3ea0d36
Gabriel Harris-Rouquette <gabizou@me.com>
2015-12-06 20:08:33 -0800

    Update Flardians.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- Plugin/Flardians/src/main/java/org/spongepowered/cookbook/Flardians.java
+++ Plugin/Flardians/src/main/java/org/spongepowered/cookbook/Flardians.java
@@ -1,22 +1,20 @@
 package org.spongepowered.cookbook;
 
 import com.google.common.collect.ImmutableList;
-import com.google.inject.Inject;
-import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.data.manipulator.mutable.DisplayNameData;
 import org.spongepowered.api.data.manipulator.mutable.entity.TradeOfferData;
 import org.spongepowered.api.data.manipulator.mutable.item.LoreData;
 import org.spongepowered.api.data.type.Careers;
 import org.spongepowered.api.data.value.mutable.ListValue;
-import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityTypes;
-import org.spongepowered.api.event.Subscribe;
-import org.spongepowered.api.event.entity.EntitySpawnEvent;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.SpawnEntityEvent;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.merchant.TradeOfferBuilder;
+import org.spongepowered.api.item.merchant.TradeOffer;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.Texts;
@@ -26,14 +24,14 @@ import org.spongepowered.api.text.format.TextStyles;
 import java.util.List;
 import java.util.Random;
 
-@Plugin(id = "flardians", name = "Flardians", version = "0.1")
+@Plugin(id = "flardians", name = "Flardians", version = "0.2")
 public class Flardians {
 
-    public static final ItemType[] SELL_TYPES = new ItemType[] {ItemTypes.SLIME_BALL, ItemTypes.HARDENED_CLAY, ItemTypes.BLAZE_ROD, ItemTypes.APPLE,
-                                                                ItemTypes.GHAST_TEAR, ItemTypes.COBBLESTONE, ItemTypes.STICK, ItemTypes.EMERALD,};
+    public static final ItemType[] SELL_TYPES = new ItemType[]{ItemTypes.SLIME_BALL, ItemTypes.HARDENED_CLAY, ItemTypes.BLAZE_ROD, ItemTypes.APPLE,
+                                                               ItemTypes.GHAST_TEAR, ItemTypes.COBBLESTONE, ItemTypes.STICK, ItemTypes.EMERALD,};
     public static final List<ItemType> BUYING_TYPES = ImmutableList.of(ItemTypes.ACACIA_DOOR, ItemTypes.LEAVES2, ItemTypes.BOOKSHELF, ItemTypes.COAL,
-                                                                      ItemTypes.COBBLESTONE, ItemTypes.ANVIL, ItemTypes.IRON_ORE, ItemTypes.APPLE,
-                                                                      ItemTypes.WHEAT_SEEDS, ItemTypes.DIRT);
+                                                                       ItemTypes.COBBLESTONE, ItemTypes.ANVIL, ItemTypes.IRON_ORE, ItemTypes.APPLE,
+                                                                       ItemTypes.WHEAT_SEEDS, ItemTypes.DIRT);
     public static final Text FLARDARIAN = Texts.of(TextColors.DARK_AQUA, TextStyles.BOLD, TextStyles.ITALIC, "Flardarian");
     public static final Text ITEM_DISPLAY = Texts.of(TextColors.YELLOW, TextStyles.BOLD, "[", TextColors.GREEN, TextStyles.ITALIC, "FLARD",
                                                      TextStyles.RESET, TextColors.YELLOW, TextStyles.BOLD, "]");
@@ -45,38 +43,37 @@ public class Flardians {
     public static final Text LORE_FIFTH = Texts.of(TextColors.BLUE, TextStyles.ITALIC, "For ", TextColors.YELLOW, "Sponge", TextColors.BLUE,
                                                    " is in a State of play");
     public static final Text LORE_SIXTH = Texts.of(TextColors.BLUE, TextStyles.ITALIC, "Today, be happy as can be!");
+    public static final ImmutableList<Text> LORE = ImmutableList.of(LORE_FIRST, LORE_SECOND, LORE_THIRD, LORE_FOURTH, LORE_FIFTH, LORE_SIXTH);
     public static final Random RANDOM = new Random();
 
-    @Inject private Game game;
-
-
-    @Subscribe
-    public void onSpawn(EntitySpawnEvent event) {
-        if (event.getEntity().getType() == EntityTypes.VILLAGER && Math.random() > 0.7) {
-            final Entity entity = event.getEntity();
-            entity.offer(Keys.CAREER, Careers.CLERIC);
-            entity.offer(Keys.DISPLAY_NAME, FLARDARIAN);
-            entity.offer(Keys.SHOWS_DISPLAY_NAME, true);
-            entity.offer(Keys.INVULNERABILITY, 10000D);
-            entity.offer(generateTradeOffer());
-        }
+    @Listener
+    public void onSpawn(SpawnEntityEvent event) {
+        event.getEntities().stream()
+            .filter(entity1 -> entity1.getType().equals(EntityTypes.VILLAGER) && Math.random() > 0.7)
+            .forEach(villager -> {
+                villager.offer(Keys.CAREER, Careers.CLERIC);
+                villager.offer(Keys.DISPLAY_NAME, FLARDARIAN);
+                villager.offer(Keys.SHOWS_DISPLAY_NAME, true);
+                villager.offer(Keys.INVULNERABILITY, 10000);
+                villager.offer(generateTradeOffer());
+            });
     }
 
     private TradeOfferData generateTradeOffer() {
         final int rand = RANDOM.nextInt(7);
         final int itemRand = RANDOM.nextInt(BUYING_TYPES.size());
 
-        final DisplayNameData itemName = game.getRegistry().getManipulatorRegistry().getBuilder(DisplayNameData.class).get().create();
+        final DisplayNameData itemName = Sponge.getManipulatorRegistry().getBuilder(DisplayNameData.class).get().create();
         itemName.set(Keys.DISPLAY_NAME, ITEM_DISPLAY);
 
         // Set up the lore data.
-        final LoreData loreData = game.getRegistry().getManipulatorRegistry().getBuilder(LoreData.class).get().create();
+        final LoreData loreData = Sponge.getManipulatorRegistry().getBuilder(LoreData.class).get().create();
         final ListValue<Text> lore = loreData.lore();
-        lore.addAll(ImmutableList.of(LORE_FIRST, LORE_SECOND, LORE_THIRD, LORE_FOURTH, LORE_FIFTH, LORE_SIXTH));
+        lore.addAll(LORE);
         loreData.set(lore);
 
         // Create the selling item
-        final ItemStack selling = game.getRegistry().getItemBuilder()
+        final ItemStack selling = ItemStack.builder()
             .itemType(BUYING_TYPES.get(itemRand))
             .itemData(itemName)
             .itemData(loreData)
@@ -84,15 +81,18 @@ public class Flardians {
             .build();
 
         // Create the buying item
-        final ItemStack buying = game.getRegistry().getItemBuilder()
+        final ItemStack buying = ItemStack.builder()
             .itemType(SELL_TYPES[rand])
             .quantity(1)
             .build();
 
-        final TradeOfferBuilder builder = game.getRegistry().getTradeOfferBuilder();
-
-        final TradeOfferData tradeOfferData = game.getRegistry().getManipulatorRegistry().getBuilder(TradeOfferData.class).get().create();
-        tradeOfferData.set(tradeOfferData.tradeOffers().add(builder.firstBuyingItem(buying).maxUses(10000).sellingItem(selling).build()));
+        final TradeOfferData tradeOfferData = Sponge.getManipulatorRegistry().getBuilder(TradeOfferData.class).get().create();
+        tradeOfferData.set(tradeOfferData.tradeOffers()
+                               .add(TradeOffer.builder()
+                                        .firstBuyingItem(buying)
+                                        .maxUses(10000)
+                                        .sellingItem(selling)
+                                        .build()));
         return tradeOfferData;
     }
 
