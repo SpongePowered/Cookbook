dd51012eb93d9deeed4b5a4fef8fdf11aff46205
Jon Stockdill <jons@kenzan.com>
2016-09-18 21:29:58 -0400

    Adding command to smite players :)

--- Plugin/Smite/src/main/java/org/spongepowered/cookbook/plugin/Smite.java
+++ Plugin/Smite/src/main/java/org/spongepowered/cookbook/plugin/Smite.java
@@ -25,9 +25,18 @@
 
 package org.spongepowered.cookbook.plugin;
 
-import com.flowpowered.math.vector.Vector3d;
-import com.google.inject.Inject;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.data.type.HandTypes;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityTypes;
@@ -39,27 +48,81 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.entity.spawn.EntitySpawnCause;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
 import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.util.blockray.BlockRay;
 import org.spongepowered.api.util.blockray.BlockRayHit;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.extent.EntityUniverse;
 
-import java.util.Optional;
+import com.flowpowered.math.vector.Vector3d;
+import com.flowpowered.math.vector.Vector3i;
+import com.google.inject.Inject;
 
-@Plugin(id = "ca.sapon.smite",
+@Plugin(id = "casaponsmite",
         name = "Smite",
         version = "1.0",
         description = "Use me on your enemies")
 public class Smite {
 
+	public static class SmiteCommand implements CommandExecutor {
+
+		private Logger logger;
+
+		public SmiteCommand(Logger logger) {
+			this.logger = logger;
+		}
+
+		@Override
+		public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+
+			Player player = args.<Player>getOne("player").get();
+			String message = args.<String>getOne("message").get();
+
+			final String name = player.getName();
+			logger.info("Executing command on player: {} with message: {}", name, message);
+
+			src.sendMessage(Text.of("Smiting " + name + "!  " + message));
+
+			Vector3i blockPosition = player.getLocation().getBlockPosition();
+			final Optional<Entity> optionalEntity = Optional
+					.of(player.getWorld().createEntity(EntityTypes.LIGHTNING, blockPosition));
+			optionalEntity.ifPresent(entity -> player.getWorld().spawnEntity(entity,
+					Cause.source(EntitySpawnCause.builder().entity(entity).type(SpawnTypes.PLUGIN).build()).build()));
+
+			return CommandResult.success();
+		}
+
+	}
+
     private static final int MAX_DISTANCE = 100;
+
     @Inject
     private PluginContainer container;
 
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onGamePreInitialization(GamePreInitializationEvent event) {
+        logger.info("Initializing smite command!!!!");
+
+        CommandSpec myCommandSpec = CommandSpec.builder()
+            .description(Text.of("Hello World Command"))
+            .permission("myplugin.command.helloworld")
+            .arguments(
+                    GenericArguments.onlyOne(GenericArguments.player(Text.of("player"))),
+                    GenericArguments.remainingJoinedStrings(Text.of("message")))
+            .executor(new SmiteCommand(logger))
+            .build();
+
+    Sponge.getCommandManager().register(this, myCommandSpec, "smite");
+
+    }
     @Listener(order = Order.POST)
     public void onInteract(InteractBlockEvent.Primary event, @First Player player) {
         // Check if the player is hitting with an ender eye and has the permissions
@@ -99,7 +162,7 @@ public class Smite {
             }
         }
         // Smite!
-        final Optional<Entity> optionalEntity = player.getWorld().createEntity(EntityTypes.LIGHTNING, closest);
+        final Optional<Entity> optionalEntity = Optional.of(player.getWorld().createEntity(EntityTypes.LIGHTNING, closest));
         optionalEntity.ifPresent(entity -> player.getWorld().spawnEntity(entity,
                 Cause.source(EntitySpawnCause.builder().entity(entity).type(SpawnTypes.PLUGIN).build()).build().merge(event.getCause())));
     }
